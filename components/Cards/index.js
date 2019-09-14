481be3eeb95433709e4741b0d1b12631599203d5
Katrina-Dierking <kalynndier@gmail.com>
2019-09-14 12:31:42 -0400

    HTTP GET request made

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,22 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+const cardsContainer = document.querySelector('.cards-container'); 
+
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles') 
+    .then((response) => { 
+        console.log(response);
+        response.data.articles.javascript.forEach(article => CreateCard(article));
+        response.data.articles.bootstrap.forEach(article => CreateCard(article));
+        response.data.articles.jquery.forEach(article => CreateCard(article));
+        response.data.articles.node.forEach(article => CreateCard(article));
+        response.data.articles.technology.forEach(article => CreateCard(article));
+    })
+
+    .catch((err) => { 
+        console.log(err) 
+    })
